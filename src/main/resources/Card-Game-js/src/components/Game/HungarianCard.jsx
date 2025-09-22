b2ec6f1654c99ac8ad665014b559dc9bb3be01c6
Shellking <b.szabiszabi@gmail.com>
2025-09-22 09:32:04 +0200

    valami

---
+++ src/main/resources/Card-Game-js/src/components/Game/HungarianCard.jsx
@@ -0,0 +1,47 @@
+import React, { memo } from 'react';
+
+function HungarianCard({cardData,onClick,isSelected }) {
+  if (!cardData) {
+    return (
+      <span  style={{
+        width: "40px",
+        height: "60px",
+        backgroundColor: "gray",
+        border: "1px solid black",
+        borderRadius: "4px",
+      }}>
+        Hátlap
+      </span>
+    );
+  }
+
+  // magyar színekhez szín hozzárendelés
+  const suitColors = {
+    HEARTS: "red",
+    ACORNS: "orange",
+    BELLS: "brown",
+    LEAVES: "green",
+  };
+
+  const color = suitColors[cardData.suit] || "black";
+
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        backgroundColor: color,
+        border: isSelected ? "3px solid yellow" : "1px solid black",
+        borderRadius: "4px",
+
+        cursor: "pointer",
+
+      }}
+    >
+      <div>{cardData.suit}</div>
+      <div>{cardData.rank}</div>
+    </button>
+  );
+
+}
+
+export default memo(HungarianCard);
